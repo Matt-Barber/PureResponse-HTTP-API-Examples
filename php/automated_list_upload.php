a790f82a318964aab8905620aa31f2896aee2a97
Matt Barber <mfmbarber@gmail.com>
2015-11-06 15:35:27 +0000

    Added PSR-2 Compliancy
    Checked using squizlabs code sniffer

--- php/automated_list_upload.php
+++ php/automated_list_upload.php
@@ -1,182 +1,189 @@
 <?php
-// An example class for creating, replacing, and appending contact lists
-// PHP 5.5
-// category Data Gathering
-// author Matt Barber
-// created_at 14/09/2015
+/**
+ * An example class for creating, replacing, and appending contact lists
+ * PHP Version 5.6+
+ * @category API
+ * @author Matt Barber <mfmbarber@gmail.com>
+**/
 
-    class Pure360Http
+class Pure360Http
+{
+    const LIST_UPLOAD_META = 'http://response.pure360.com/interface/list_upload_meta.php';
+    const LIST_UPLOAD_DATA = 'http://response.pure360.com/interface/list_upload_data.php';
+
+    /**
+     * Creates a new contact list in the given profile (secured by the token)
+     *
+     * @param string $profileName  The name of the profile
+     * @param string $token        Security token securing the list uploads
+     * @param string $responseType Type of response for the upload notification
+     * @param string $responseUri  Where to send the notification
+     * @param string $listName     Name of the list to create in the platform
+     * @param string $filename     File path and name of the file to upload
+     *
+     * @return string
+    **/
+    public static function createList(
+        $profileName,
+        $token,
+        $responseType,
+        $responseUri,
+        $listName,
+        $filename
+    ) {
+        $params = [
+            'profileName' => $profileName,
+            'token' => $token,
+            'responseType' => $responseType,
+            'responseUri' => $responseUri,
+            'listName' => $listName,
+            'transactionType' => 'CREATE'
+        ];
+        return SELF::_processList($params, $filename);
+    }
+    /**
+     * Replaces a contact list in the given profile (secured by the token)
+     *
+     * @param string $profileName  The name of the profile
+     * @param string $token        Security token securing the list uploads
+     * @param string $responseType Type of response for the upload notification
+     * @param string $responseUri  Where to send the notification
+     * @param string $listName     Name of the list to create in the platform
+     * @param string $filename     File path and name of the file to upload
+     *
+     * @return string
+    **/
+    public static function replaceList(
+        $profileName,
+        $token,
+        $responseType,
+        $responseUri,
+        $listName,
+        $filename
+    ) {
+        $params = [
+            'profileName' => $profileName,
+            'token' => $token,
+            'responseType' => $responseType,
+            'responseUri' => $responseUri,
+            'listName' => $listName,
+            'transactionType' => 'REPLACE'
+        ];
+        return SELF::_processList($params, $filename);
+    }
+        /**
+     * Appends (updates / inserts to) a contact list in the given profile
+     *
+     * @param string $profileName  The name of the profile
+     * @param string $token        Security token securing the uploads
+     * @param string $responseType The type of response for the notification
+     * @param string $responseUri  Where to send the notification
+     * @param string $listName     Name of the list to create in the platform
+     * @param string $filename     File path and name of the file to upload
+     *
+     * @return string
+    **/
+    public static function appendList(
+        $profileName,
+        $token,
+        $responseType,
+        $responseUri,
+        $listName,
+        $filename
+    ) {
+        $params = [
+            'profileName' => $profileName,
+            'token' => $token,
+            'responseType' => $responseType,
+            'responseUri' => $responseUri,
+            'listName' => $listName,
+            'transactionType' => 'APPEND'
+        ];
+        return SELF::_processList($params, $filename);
+    }
+    /**
+     * Fed by the public classes this uploads the contact list
+     *
+     * @param array  $params   The query parameters
+     * @param string $filename filepath and name of the file to upload
+     *
+     * @return string
+    **/
+    private static function _processList($params, $filename)
     {
-        const LIST_UPLOAD_META = 'http://response.pure360.com/interface/list_upload_meta.php';
-        const LIST_UPLOAD_DATA = 'http://response.pure360.com/interface/list_upload_data.php';
-
-        public function __construct()
-        {
-        }
-        /**
-         * Creates a new contact list in the given profile (secured by the token)
-         * @param profileName   string      The name of the profile to create the list in
-         * @param token         string      Security token securing the list uploads for the profile
-         * @param responseType  string      The type of response for the upload notification
-         * @param responseUri   string      Where to send the notification
-         * @param listName      string      The name of the list to create in the platform
-         * @param filename      string      filepath and name of the file to upload
-        **/
-        public function createList(
-            $profileName,
-            $token,
-            $responseType,
-            $responseUri,
-            $listName,
-            $filename
-        )
-        {
-            $params = [
-                'profileName' => $profileName,
-                'token' => $token,
-                'responseType' => $responseType,
-                'responseUri' => $responseUri,
-                'listName' => $listName,
-                'transactionType' => 'CREATE'
-            ];
-            return $this->processList($params, $filename);
-        }
-        /**
-         * Replaces a contact list in the given profile (secured by the token)
-         * @param profileName   string      The name of the profile to create the list in
-         * @param token         string      Security token securing the list uploads for the profile
-         * @param responseType  string      The type of response for the upload notification
-         * @param responseUri   string      Where to send the notification
-         * @param listName      string      The name of the list to replace in the platform
-         * @param filename      string      filepath and name of the file to upload
-        **/
-        public function replaceList(
-            $profileName,
-            $token,
-            $responseType,
-            $responseUri,
-            $listName,
-            $filename
-        )
-        {
-            $params = [
-                'profileName' => $profileName,
-                'token' => $token,
-                'responseType' => $responseType,
-                'responseUri' => $responseUri,
-                'listName' => $listName,
-                'transactionType' => 'REPLACE'
-            ];
-            return $this->processList($params, $filename);
-        }
-
-        /**
-         * Appends (updates / inserts to) a contact list in the given profile (secured by the token)
-         * @param profileName   string      The name of the profile to create the list in
-         * @param token         string      Security token securing the list uploads for the profile
-         * @param responseType  string      The type of response for the upload notification
-         * @param responseUri   string      Where to send the notification
-         * @param listName      string      The name of the list to append in the platform
-         * @param filename      string      filepath and name of the file to upload
-        **/
-        public function appendList(
-            $profileName,
-            $token,
-            $responseType,
-            $responseUri,
-            $listName,
-            $filename
-        )
-        {
-            $params = [
-                'profileName' => $profileName,
-                'token' => $token,
-                'responseType' => $responseType,
-                'responseUri' => $responseUri,
-                'listName' => $listName,
-                'transactionType' => 'APPEND'
-            ];
-            return $this->processList($params, $filename);
-        }
-        /**
-         * Fed by the public classes this uploads the contact list
-         * @param params    array       Array of key value pairs that make up the query parameters
-         * @param filename  string      filepath and name of the file to upload
-        **/
-        private function processList($params, $filename)
-        {
-            $params = array_merge($params, $this->getHeaderIndexes($filename));
-            $transactionId = trim(
-                array_pop(
-                    explode(
-                        ':', $this->webRequest($params, self::LIST_UPLOAD_META)
-                    )
+        $params = array_merge($params, $this->getHeaderIndexes($filename));
+        $transactionId = trim(
+            array_pop(
+                explode(
+                    ':', $this->webRequest($params, self::LIST_UPLOAD_META)
                 )
-            );
-            $params = [
-                'transactionId' => $transactionId,
-                'profileName' => $params['profileName']
-            ];
-            return $this->webRequest($params, self::LIST_UPLOAD_DATA, $filename);
-        }
-        /**
-         * Converts the first line of a CSV into the required meta data
-         * @param filename  string  filepath and name of the file to upload
-        **/
-        private function getHeaderIndexes($filename)
-        {
-            $meta_data = [];
-            if (($handle = fopen($filename, 'r')) !== FALSE)
-            {
-                $line = array_flip(fgetcsv($handle, 1000, ','));
-                foreach ($line as $key => $value)
-                {
-                    if (
-                        (stristr($key, 'email')!==FALSE) &&
-                        (!isset($meta_data['emailCol']))
-                    )
-                    {
-                        $meta_data['emailCol'] = $value;
-                    }
-                    else
-                    {
-                        $meta_data["COL_{$key}"] = $value;
-                    }
+            )
+        );
+        $params = [
+            'transactionId' => $transactionId,
+            'profileName' => $params['profileName']
+        ];
+        return SELF::_webRequest($params, self::LIST_UPLOAD_DATA, $filename);
+    }
+    /**
+     * Converts the first line of a CSV into the required meta data
+     *
+     * @param string $filename filepath and name of the file to upload
+     *
+     * @return array
+    **/
+    private static function _getHeaderIndexes($filename)
+    {
+        $meta_data = [];
+        if (($handle = fopen($filename, 'r')) !== false) {
+            $line = array_flip(fgetcsv($handle, 1000, ','));
+            foreach ($line as $key => $value) {
+                $has_email = stristr($key, 'email');
+                $email_set = isset($meta_data['emailCol']);
+                if ($has_email && !$email_set) {
+                    $meta_data['emailCol'] = $value;
+                } else {
+                    $meta_data["COL_{$key}"] = $value;
                 }
             }
-            fclose($handle);
-            return $meta_data;
         }
-
-        /**
-         * This sends the web request using curl and CURLFile to the endpoint
-         * with the query parameters
-         * @param payload       array   Query parameters
-         * @param url           string  End point
-         * @param upload_file   string  filepath and name of the file to upload
-        **/
-        private function webRequest($payload, $url, $upload_file=null)
-        {
-            $curl = curl_init();
-            if($upload_file !== null)
-            {
-                // Converts the filename and path to a CURLFile
-                if(stristr($upload_file, './') !== FALSE)
-                    $upload_file = realpath('./') . '/' . basename($upload_file);
-                $payload['file'] = new CURLFile("{$upload_file}");
-            }
-            curl_setopt_array($curl, [
-                CURLOPT_URL => $url,
-                CURLOPT_HEADER => FALSE,
-                CURLOPT_RETURNTRANSFER => TRUE,
-                CURLOPT_POST => TRUE,
-                CURLOPT_POSTFIELDS => $payload
-            ]);
-            $response = CURL_EXEC($curl);
-            if ($response === FALSE)
-                throw new exception(CURL_ERROR($curl), CURL_ERRNO($curl));
-            curl_close($curl);
-            return $response;
-
-        }
+        fclose($handle);
+        return $meta_data;
     }
+    /**
+     * This sends the web request using curl and CURLFile to the endpoint
+     *
+     * @param array  $payload     Query parameters
+     * @param string $url         End point
+     * @param string $upload_file File path and name of the file to upload
+     *
+     * @return string
+    **/
+    private static function _webRequest($payload, $url, $upload_file=null)
+    {
+        $curl = curl_init();
+        if ($upload_file !== null) {
+            // Converts the filename and path to a CURLFile
+            if (stristr($upload_file, './') !== false) {
+                $upload_file = realpath('./') . '/' . basename($upload_file);
+            }
+            $payload['file'] = new CURLFile("{$upload_file}");
+        }
+        curl_setopt_array(
+            $curl,
+            [
+                CURLOPT_URL => $url,
+                CURLOPT_HEADER => false,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_POST => true,
+                CURLOPT_POSTFIELDS => $payload
+            ]
+        );
+        $response = CURL_EXEC($curl);
+        if ($response === false) {
+            throw new exception(CURL_ERROR($curl), CURL_ERRNO($curl));
+        }
+        curl_close($curl);
+        return $response;
+    }
+}
